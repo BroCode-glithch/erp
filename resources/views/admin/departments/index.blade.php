18d4a3cd6896a47baaeff2cd096bd0a33c0861a7
BroCode-glithch <ariyomiracle1234@gmail.com>
2025-07-14 03:30:46 +0100

    fixed create department route

--- resources/views/admin/departments/index.blade.php
+++ resources/views/admin/departments/index.blade.php
@@ -59,7 +59,7 @@
                     class="px-3 py-2 text-sm font-medium text-white bg-yellow-500 rounded-md hover:bg-yellow-600">
                     Export XML
                     </a>
-                    <a href="{{ route('admin.permissions.create') }}"
+                    <a href="{{ route('admin.departments.create') }}"
                     class="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700">
                     + New Department
                     </a>
